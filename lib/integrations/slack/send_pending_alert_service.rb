1e912c152a7bb536a21abc10b11df478a7e77594
Daniil Sigal <d@sigal.me>
2026-03-19 22:51:46 +0000

    fix(slack): store alert thread ts in additional_attributes to avoid collision with identifier

--- lib/integrations/slack/send_pending_alert_service.rb
+++ lib/integrations/slack/send_pending_alert_service.rb
@@ -4,13 +4,13 @@ class Integrations::Slack::SendPendingAlertService
   def perform
     return unless conversation.open?
     return if hook.reference_id.blank?
-    return if conversation.identifier.present?
+    return if conversation.additional_attributes['pending_alert_thread_ts'].present?
 
     response = slack_client.chat_postMessage(
       channel: hook.reference_id,
       text: alert_text
     )
-    conversation.update!(identifier: response['ts'])
+    conversation.update!(additional_attributes: conversation.additional_attributes.merge('pending_alert_thread_ts' => response['ts']))
   rescue Slack::Web::Api::Errors::IsArchived, Slack::Web::Api::Errors::AccountInactive, Slack::Web::Api::Errors::MissingScope,
          Slack::Web::Api::Errors::InvalidAuth,
          Slack::Web::Api::Errors::ChannelNotFound, Slack::Web::Api::Errors::NotInChannel => e
